70872386e67938cb896277e0b8855702506c2fbb
khalid-kifayat <khalid.kifayat@gmail.com>
2024-02-07 16:20:40 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from dotenv import load_dotenv
+load_dotenv() ## loading all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## function to load Gemini Pro model and get repsonses
+model=genai.GenerativeModel("gemini-pro") 
+chat = model.start_chat(history=[])
+def get_gemini_response(question):
+    
+    response=chat.send_message(question,stream=True)
+    return response
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="Q&A SaveChatHistory")
+
+st.header("Q\A Saving Chat History - Gemini Pro LLM App")
+
+# Initialize session state for chat history if it doesn't exist
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+input=st.text_input("Input: ",key="input")
+submit=st.button("Ask the question")
+
+if submit and input:
+    response=get_gemini_response(input)
+    # Add user query and response to session state chat history
+    st.session_state['chat_history'].append(("You", input))
+    st.subheader("The Response is")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot", chunk.text))
+st.subheader("The Chat History is")
+    
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
+ 
+st.markdown("---")
+st.caption("using Google Gemini Pro, Python-Langchain Application created by Khalid kifayat :sunglasses:")
+
+hide_streamlit_style = """
+            <style>
+
+            [data-testid="stToolbar"] {visibility: hidden;}
+            .reportview-container {
+            margin-top: -2em;
+        }
+            #MainMenu {visibility: hidden;}
+            .stDeployButton {display:none;}
+            #stDecoration {display:none;}
+            footer {visibility: hidden;}
+            div.embeddedAppMetaInfoBar_container__DxxL1 {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+    
