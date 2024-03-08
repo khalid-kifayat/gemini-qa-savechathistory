707ca179739e628f0ff1189ce629584a4632d31e
khalid-kifayat <khalid.kifayat@gmail.com>
2024-03-08 08:08:05 +0500

    code commit

--- app.py
+++ app.py
@@ -42,6 +42,26 @@ st.subheader("Answer: ")
 for role, text in st.session_state['chat_history']:
     st.write(f"{role}: {text}")
  
+# Footer
+st.markdown("""
+<style>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #000000; /* Black background color */
+    color: #ffffff; /* White text color */
+    text-align: center;
+    padding: 10px;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown("---")
+st.markdown('<p class="footer">Generative AI : Python-Langchain Application <br>  Created by Khalid Kifayat <br> (www.beingkhalid.com / www.builtautomations.com)</p>', unsafe_allow_html=True)
+
+
 hide_streamlit_style = """
             <style>
 
